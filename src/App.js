008495543613c9b8150ed0476ff3077217ad88f7
lkrabac <lkrabac1@gmail.com>
2022-12-31 18:53:07 +0100

    Header, Footer +  pridané v App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Header, Footer } from './page-components';
 
 function App() {
   return (
     <>
+    <Header />
+    <div>
+      <h1>Body</h1>
+    </div>
+    <Footer />
     </>
   );
 }
