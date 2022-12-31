900d57d03c543c9d04aba9b507b80c429c9a4f32
lkrabac <lkrabac1@gmail.com>
2022-12-31 19:20:53 +0100

    + router setup // Setup je komplet.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 
+import {Routes,Route} from 'react-router-dom';
+
 import { Header, Footer } from './page-components';
 import { Homepage } from './pages';
 
 function App() {
   return (
     <>
-    <Header />
-    <Homepage />
-    <Footer />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+      <Footer />
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {BrowserRouter} from 'react-router-dom';
+
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
@@ -9,10 +11,10 @@ const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
