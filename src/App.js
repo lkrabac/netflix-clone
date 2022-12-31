8d56c7edd6cacb6fd92d9171339d3c3fa07d9759
lkrabac <lkrabac1@gmail.com>
2022-12-31 19:01:10 +0100

    + Homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+
 import { Header, Footer } from './page-components';
+import { Homepage } from './pages';
 
 function App() {
   return (
     <>
     <Header />
-    
+    <Homepage />
     <Footer />
     </>
   );
